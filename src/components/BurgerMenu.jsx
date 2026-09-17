3f1bc2b83bc158748cc75fb9942e9ebf0a29095a
shamtkh <tohirovsham@gmail.com>
2026-09-17 19:47:28 +0500

    Update burger menu animation

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -2,15 +2,17 @@ import './burgerMenu.css'
 
 export default function BurgerMenu({ checked, onChange }) {
   return (
-    <label className="burger-menu" aria-label={checked ? 'Close menu' : 'Open menu'}>
+    <label className="burger-menu container" aria-label={checked ? 'Close menu' : 'Open menu'}>
       <input
         type="checkbox"
         checked={checked}
         onChange={(event) => onChange(event.target.checked)}
       />
-      <span className="top" />
-      <span className="middle" />
-      <span className="bottom" />
+      <span className="checkmark">
+        <span />
+        <span />
+        <span />
+      </span>
     </label>
   )
 }
