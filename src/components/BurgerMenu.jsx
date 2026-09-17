6f7706454e2f025fc2ddfdc7d4f8bfba277e6351
shamtkh <tohirovsham@gmail.com>
2026-09-17 20:18:31 +0500

    Restore standalone burger menu

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -1,18 +1,18 @@
 import './burgerMenu.css'
 
 export default function BurgerMenu({ checked, onChange }) {
-  return (
-    <label className="burger-menu container" aria-label={checked ? 'Close menu' : 'Open menu'}>
-      <input
-        type="checkbox"
-        checked={checked}
-        onChange={(event) => onChange(event.target.checked)}
-      />
-      <span className="checkmark">
-        <span />
-        <span />
-        <span />
-      </span>
-    </label>
-  )
+	return (
+		<label className="burger-menu" aria-label={checked ? 'Close menu' : 'Open menu'}>
+			<input
+				type="checkbox"
+				checked={checked}
+				onChange={(event) => onChange(event.target.checked)}
+			/>
+			<span className="checkmark">
+				<span />
+				<span />
+				<span />
+			</span>
+		</label>
+	)
 }
